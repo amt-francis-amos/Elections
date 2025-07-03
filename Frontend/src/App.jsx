da9cc82d2c872d1a0bf1a9fb582a67db4e6e182c
amt-francis-amos <sugartone45@yahoo.com>
2025-07-03 00:20:06 -0800

    updated

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,21 +1,19 @@
 import React from 'react'
-import Navbar from './components/Navbar'
 import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Vote from './pages/Vote'
-
-
 
 const App = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-     <div className="flex-grow">
-       <Routes>
-        <Route path='/'  element={<Home />} />
-        <Route path='/vote'  element={<Vote />} />
-      </Routes>
-     </div>
+      <div className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/vote" element={<Vote />} />
+        </Routes>
+      </div>
     </div>
   )
 }
