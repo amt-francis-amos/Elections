f93de4dce07ebd7b4c6c2ecdde3e1659ed3f5a38
amt-francis-amos <sugartone45@yahoo.com>
2025-07-24 20:22:51 -0800

    updated

--- Backend/config/mongoDB.js
+++ Backend/config/mongoDB.js
@@ -1,8 +1,16 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const connectDB = async () => {
-  mongoose.connection.on("connected", () => console.log("Database Connected"));
-  mongoose.connect(`${process.env.MONGO_URI}/Nsbt-DB`);
+const connectDb = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      dbName: "Nsbt-Db",
+    });
+
+    console.log("✅ Connected to DB:", conn.connection.name);
+  } catch (error) {
+    console.error("❌ Error connecting to the database:", error.message);
+    process.exit(1);
+  }
 };
 
-export default connectDB;
+export default connectDb;
