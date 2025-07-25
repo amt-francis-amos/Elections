16af58d153cab48ce34a65adafcd4a90d1f82199
amt-francis-amos <sugartone45@yahoo.com>
2025-07-25 13:37:29 -0800

    updated

--- Backend/routes/candidateRoutes.js
+++ Backend/routes/candidateRoutes.js
@@ -1,12 +1,12 @@
 
 import express from 'express';
 import { getCandidatesByElection, addCandidate } from '../controllers/candidateController.js';
-import { authenticateToken } from '../middlewares/auth.js';
+import { auth } from '../middlewares/auth.js';
 import upload from '../middlewares/multer.js';
 
 const router = express.Router();
 
-router.get('/candidates/:electionId',  authenticateToken, getCandidatesByElection);
-router.post('/candidates',  authenticateToken, upload.single('image'), addCandidate);
+router.get('/candidates/:electionId',  auth, getCandidatesByElection);
+router.post('/candidates',  auth, upload.single('image'), addCandidate);
 
 export default router;
