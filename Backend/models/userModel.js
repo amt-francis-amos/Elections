21c7f53de34298f68c3b31ff920f79780d123641
amt-francis-amos <sugartone45@yahoo.com>
2025-07-25 06:35:34 -0800

    updated

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema(
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    userId: { type: String, unique: true }, 
+    userId: { type: String, unique: true, required: true }, 
   },
   {
     timestamps: true,
