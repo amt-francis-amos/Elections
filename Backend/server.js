e832dc8050953e21c810036e819a7c3e193ffed2
amt-francis-amos <sugartone45@yahoo.com>
2025-07-25 06:31:14 -0800

    updated

--- Backend/server.js
+++ Backend/server.js
@@ -1,28 +1,25 @@
-import express from 'express'
-import cors from 'cors'
-import 'dotenv/config'
-import userRoutes from './routes/userRoutes.js'
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import userRoutes from './routes/userRoutes.js';
 import connectDb from './config/mongoDB.js';
+
 const app = express();
-
-
-
 const PORT = process.env.PORT || 5000;
+
 
 connectDb();
 
 
- app.use(cors({credentials:true}));
- app.use(express.json());
-
-app.use('/api/users', userRoutes)
+app.use(cors({ credentials: true, origin: true })); 
+app.use(express.json());
 
 
-
+app.use('/api/users', userRoutes);
 
 
 
 
-app.listen(PORT, ()=> {
-    console.log(`Server is running on PORT ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`✅ Server is running on http://localhost:${PORT}`);
+});
