a5204e3093aa1636d95c23423e6c16df753ffd17
amt-francis-amos <sugartone45@yahoo.com>
2025-07-24 18:02:27 -0800

    updated

---
+++ Backend/server.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+
+ app.use(cors({credential:true}));
+ app.use(express.json());
+
+
+
+
+
+
+app.listen(PORT, ()=> {
+    console.log(`Server is running on PORT ${PORT}`)
+})
