b7e5e965dad59af29e643edab7acd31fdb19ebdc
amt-francis-amos <sugartone45@yahoo.com>
2025-07-27 15:18:43 -0800

    updated

--- Backend/routes/candidateRoutes.js
+++ Backend/routes/candidateRoutes.js
@@ -6,7 +6,6 @@ import upload from '../middlewares/multer.js';
 
 const router = express.Router();
 
-
 const validateElectionId = (req, res, next) => {
   const { electionId } = req.params;
   if (!mongoose.Types.ObjectId.isValid(electionId)) {
@@ -15,10 +14,7 @@ const validateElectionId = (req, res, next) => {
   next();
 };
 
-
 router.get('/candidates/:electionId', auth, validateElectionId, getCandidatesByElection);
-
-
 router.post('/candidates', auth, upload.single('image'), addCandidate);
 
 export default router;
