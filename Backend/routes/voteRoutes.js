41858101af6f61c0ffa7c56a60c749ae444cfa5e
amt-francis-amos <sugartone45@yahoo.com>
2025-07-28 01:56:30 -0800

    updated

--- Backend/routes/voteRoutes.js
+++ Backend/routes/voteRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import { castVote, getResults } from '../controllers/voteController.js';
 import auth from '../middlewares/auth.js';
-import { authorizeRoles } from '../middlewares/authorizeRoles.js';
+
 
 const router = express.Router();
 
 router.post('/', auth, castVote);
-router.get('/:electionId/results', auth, authorizeRoles('admin'), getResults);
+
+
+router.get('/:electionId/results', auth, getResults);
 
 export default router;
