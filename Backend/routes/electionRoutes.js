eaf682ac2f9a07e82b5ad4bf0c8db727a1ae4a18
amt-francis-amos <sugartone45@yahoo.com>
2025-07-29 09:27:59 -0800

    updated

--- Backend/routes/electionRoutes.js
+++ Backend/routes/electionRoutes.js
@@ -1,12 +1,21 @@
 import express from 'express';
-import { createElection, getAllElections } from '../controllers/electionController.js';
-import  auth  from '../middlewares/auth.js';
+import { 
+  createElection, 
+  getAllElections, 
+  updateElection, 
+  deleteElection, 
+  getElectionById 
+} from '../controllers/electionController.js';
+import auth from '../middlewares/auth.js';
 import { authorizeRoles } from '../middlewares/authorizeRoles.js';
 
 const router = express.Router();
 
 
 router.post('/', auth, authorizeRoles('admin'), createElection);
-router.get('/', auth, getAllElections); 
+router.get('/', auth, getAllElections);
+router.get('/:id', auth, getElectionById);
+router.put('/:id', auth, authorizeRoles('admin'), updateElection);
+router.delete('/:id', auth, authorizeRoles('admin'), deleteElection);
 
 export default router;
